11539fad64984424ef764575e2e93a0edd13dd85
github username <github email address>
2020-02-06 17:38:32 -0800

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,24 @@
 class Car
-
+  attr_accessor :classification, :owner, :mechanic
   attr_reader :make, :model
+  @@all = []
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map { |car| car.classification }
+  end
+
+  def possible_mechanics
+    Mechanic.all.select { |mechanic| mechanic.classification == self.classification }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,16 @@ def reload
   load 'config/environment.rb'
 end
 
+a = Car.new("VW", "passat", 'awesome')
+a.owner= (bob)
+b = Car.new("VW", 'jetta', 'old')
+b.owner=(bob)
+c = Car.new('Mercedes', "x", 'diesel')
+c.owner=(joe)
+
+bob = CarOwner.new("Bob")
+joe = CarOwner.new("Joe")
+
+
+
 binding.pry
